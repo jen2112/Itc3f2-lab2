f34527db18ca8dea11518b5d5d4e27dd5e7697dc
Ramirez, Jennifer M <144778693+jen2112@users.noreply.github.com>
2023-09-25 00:45:30 +0800

    Create create.php

---
+++ app/Views/create.php
@@ -0,0 +1,32 @@
+<div class="modal" id="CreatePlaylist">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h4 class="modal-title">Create New Playlist</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+          </div>
+
+          <!-- Modal body -->
+          <div class="modal-body">
+          <form action="/save" method="post">
+            <!-- <p id="modalData"></p> -->
+            <input type="hidden" id="IdMusic" name="IdMusic">
+            <input  name="PlayList" class="form-control" >
+
+              
+
+           
+            <input type="submit" name="Save">
+            </form>
+          </div>
+
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
